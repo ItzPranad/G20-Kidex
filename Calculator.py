39e81f80702a48d571fba87f20c868fd2fc1c94b
ItzPranad <135993411+ItzPranad@users.noreply.github.com>
2023-06-12 17:44:26 +0530

    Add files via upload
    Hello I am Pranad And I didnt knew what to Submit so before i had submitted my own Python Projects Which i have made so This Is the Real Kidex assignment Write a Python Program

---
+++ Calculator.py
@@ -0,0 +1,15 @@
+num1=int(input("Enter a value : "))
+num2=int(input("Enter the value : "))
+opr=input("Enter the Opr...")
+if opr=="+":
+    print(num1+num2)
+elif opr=="-":
+    print(num1-num2)
+elif opr=="*":
+    print(num1*num2)
+elif opr=="/":
+    print(num1/num2)
+else:
+    print("Invalid Opreation ")
+
+
